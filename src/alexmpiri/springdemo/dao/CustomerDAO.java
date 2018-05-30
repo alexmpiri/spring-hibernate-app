2d2b2a803b613fd4a642d192bbc0116cb42f045d
alexmpiri <alexmpiri@gmail.com>
2018-05-30 17:15:58 +0300

    Create DAO Interface and provide the DAO implementation for Customer

---
+++ src/alexmpiri/springdemo/dao/CustomerDAO.java
@@ -0,0 +1,11 @@
+package alexmpiri.springdemo.dao;
+
+import java.util.List;
+
+import alexmpiri.springdemo.entity.Customer;
+
+public interface CustomerDAO {
+	
+	public List<Customer> getCustomers();
+
+}
